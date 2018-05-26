087dbabf6c7ae1c4a672be1874b644d87423cffe
asphyxia <asphyxia@novusphere.io>
2018-05-25 23:13:09 -0400

    Updates

--- static/js/jquery.tubular.1.0.js
+++ static/js/jquery.tubular.1.0.js
@@ -131,7 +131,7 @@
     // load yt iframe js api
 
     var tag = document.createElement('script');
-    tag.src = "http://www.youtube.com/iframe_api";
+    tag.src = "https://www.youtube.com/iframe_api";
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
