b872606e0f61ff0ca21c503192e9902489de5aa8
asphyxia <asphyxia@novusphere.io>
2018-05-26 15:34:23 -0400

    logo and text fixes

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -5,8 +5,8 @@
             <div class="container">
                 <h2>The Technology</h2>
                 <p class="title">
-                  Novusphere provides an immutable liability-free blockchain based database which application developers can use to build censorship resistant platforms. 
-                  We believe that freedom of speech as well as other rights are gradually being eroded and seek to address these issues. 
+                  Novusphere provides an immutable liability-free blockchain based database which developers can use to build censorship resistant platforms. 
+                  We believe that freedom of speech and expression as well as other rights are gradually being eroded and seek to address these issues. 
                   <br>
                   <strong>Any traditional application which relies on a centralized database can use Novusphere as an alternative.</strong>
                 </p>
@@ -15,22 +15,22 @@
                     <div class="col-md-3 col-sm-6">
                         <h1><i class="fas fa-balance-scale"></i></h1>
                         <h5>Free Usage</h5>
-                        <p>Novusphere is entirely free to use, no mandatory paywalls to access content you can otherwise find elsewhere for free</p>
+                        <p>Novusphere is entirely free to use, no mandatory paywalls to access content you can otherwise find elsewhere for free.</p>
                     </div>
                     <div class="col-md-3 col-sm-6">
                         <h1><i class="fas fa-globe"></i></h1>
                         <h5>Decentralized Governance</h5>
-                        <p>Important decisions are made final by ATMOS holders, the community governs the future development of the platform</p>
+                        <p>Important decisions are made final by ATMOS holders, the community governs the future development of the platform.</p>
                     </div>
                     <div class="col-md-3 col-sm-6">
                         <h1><i class="fas fa-code"></i></h1>
                         <h5>Open Source</h5>
-                        <p>We strive to produce open source software that is free to use and anyone can audit</p>
+                        <p>We strive to produce open source software that is free to use and anyone can audit.</p>
                     </div>
                     <div class="col-md-3 col-sm-6">
                         <h1><i class="fas fa-unlock"></i></h1>
                         <h5>Uncensorable</h5>
-                        <p>Nobody can prevent you from sharing the content you want to share, or expressing the thoughts that you have</p>
+                        <p>Nobody can prevent you from adding entries to the database. That means sharing the content you want to share, or expressing the thoughts that you have!</p>
                     </div>
                 </div>
             </div>
